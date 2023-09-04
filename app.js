65fd4a66fdcee7721338e7f1d989bc0839d328ea
MariaRipley <roblesb.maria@gmail.com>
2023-09-04 13:32:56 +0200

    Same but using selectors inside the element

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict';
 
-const btns = document.querySelectorAll('.question-btn');
+// Using selectors inside the element
 
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(ev) {
-        const question = ev.currentTarget.parentElement.parentElement;
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function() {
         question.classList.toggle('show-text');
-    });
+    })
 });
+
+// Traversing the DOM
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(ev) {
+//         const question = ev.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
