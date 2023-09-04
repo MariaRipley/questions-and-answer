6a33ca0862c44d88c723ba9d6392b31d195aa4b6
MariaRipley <roblesb.maria@gmail.com>
2023-09-04 13:25:49 +0200

    Add forEach loop with listener to show/hide text

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(ev) {
+        const question = ev.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
+});
